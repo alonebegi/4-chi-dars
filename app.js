c00f0b6160428a1d8ff56461d29daebf07f924d4
Sunnatillo Azimjonov <monomusix001@gmail.com>
2024-03-05 09:54:55 +0300

    homeworkMasala

---
+++ app.js
@@ -0,0 +1,139 @@
+//sonni br brga qoshish
+// function summa(x, y) {
+//   console.log(x + y);
+// }
+// summa(10, 5);
+
+//sonni bitaga kopaytrsh
+// let son = +prompt("sonni kriting : ");
+// function resualt() {
+//   console.log((son += 1));
+//   return son;
+// }
+// resualt(son);
+
+//uchburchakni yuzini topish
+// function burchak(x, y) {
+//   let s = (x + y) * 2;
+//   console.log(s);
+//   return s;
+// }
+
+// burchak(2, 4);
+
+// yoshni topish
+// function year(yosh) {
+//   let yoshi = yosh * 365;
+//   console.log(yoshi);
+//   return yoshi;
+// }
+// year(20);
+
+// darajani topish
+// function kopaytr(son) {
+//   let kvadrat = son ** 2;
+//   let kub = son ** 3;
+//   let daraja = son ** 10;
+//   console.log(kvadrat, kub, daraja);
+// }
+// kopaytr(2);
+
+// 1 chisini topb beradi
+// let sons = [200, 1, 300];
+// function son() {
+//   son = sons[2];
+//   console.log(son);
+//   return son;
+// }
+// son();
+
+// 8masala
+// 2 ta sonni kopaytradi
+// function kopaytma(x, y) {
+//   let kopaytma = x * y;
+//   console.log(kopaytma);
+// }
+// kopaytma(230, 10);
+
+// 9masala
+// soat ni  sekund qilish
+// function soats(x) {
+//   let daqiqa = x * 60;
+//   let soat = daqiqa * 60;
+//   console.log(soat);
+// }
+// soats(2);
+
+// HOMEWORK
+// HOMEWORK
+// HOMEWORK
+// HOMEWORK
+// HOMEWORK
+// HOMEWORK
+// HOMEWORK
+// HOMEWORK
+// HOMEWORK
+
+// 10 masala
+// uchburchakni  tomon uzunligini topish
+// function uchinchiTomon(tomon1, tomon2) {
+//   tomon1 = +prompt("brinchi tomoni kriting : ");
+//   tomon2 = +prompt("tomon 2 ni kriting");
+//   let tomon3 = tomon1 + tomon2 - 1;
+//   console.log(tomon3);
+//   return tomon3;
+// }
+// uchinchiTomon();
+
+// 11masala
+// function qoldiq(son1, son2) {
+//   son1 = +prompt("son 1 ni kriting : ");
+//   son2 = +prompt("son 2 ni kriting");
+//   let son3 = son1 % son2;
+//   console.log(son3);
+//   return son3;
+// }
+// qoldiq();
+
+// 12masala
+// totburchakni yuzini topish
+// function turtburchakYuzi(boyi, eni) {
+//   boyi = +prompt("boyini kiriting : ");
+//   eni = +prompt("enini kiriting : ");
+//   let yuzi = boyi * 2 + eni * 2;
+//   console.log(yuzi);
+//   return yuzi;
+// }
+// turtburchakYuzi();
+
+// stringa qp matnga qoshish
+// 13masala
+// function stringQoshish(a) {
+//   a = prompt("matn yozing : ");
+//   let resualt = "somethink" + " " + a;
+//   console.log(resualt);
+//   return resualt;
+// }
+// stringQoshish();
+
+// masala 14
+// soni darajasini topish
+// function daraja(daraja) {
+//   daraja = +prompt("sonni kriting : ");
+//   let kvadrati = daraja ** 2;
+//   console.log(kvadrati);
+//   return kvadrati;
+// }
+// daraja();
+
+// masala 15
+// 0 dan kota yo kichikligini aniqlash
+// function noldan(raqam) {
+//   raqam = +prompt("sonni kriting :");
+//   if ((raqam = 0 || raqam > 0)) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// noldan();
